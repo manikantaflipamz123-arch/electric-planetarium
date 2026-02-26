a2a4976104dabc364bf2c823baa2ca1af00c7e61
Srinivas-Upl <srinivasrocks7@gmail.com>
2026-02-26 17:19:12 +0530

    Phase 23: Increase api/products payload size to 4mb to support image uploads

--- api/products.js
+++ api/products.js
@@ -1,6 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { query } from '../utils/db.js';
 
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '4mb'
+        }
+    }
+};
+
 export default async function handler(req, res) {
     const { method } = req;
 
